e4452cf2495674bbf28e406494087ff9fce06cbf
brodieG <brodieG@users.noreply.github.com>
2018-10-20 17:25:41 -0400

    initial commit

---
+++ R/pure-shade.R
@@ -0,0 +1,98 @@
+#' Pure Base R Version of rayshader
+#'
+#' This is derived from the Wolf Vollprecht's
+#' [adaptation](https://nextjournal.com/wolfv/how-fast-is-r-with-fastr-pythran)
+#' of the original pure R implementation from Tyler Morgan Wall's [blog
+#' post](http://www.tylermw.com/throwing-shade/).
+#'
+#' @export
+#' @inheritParams rayshader::ray_shade
+#' @value numeric matrix of shadow intensities between with values in
+#'   &91;0,1&93;.
+
+ray_shade2 <- function(
+  heightmap, anglebreaks=seq(40, 50, 1), sunangle=315, maxsearch=100
+) {
+  anglebreaks <- sort(tan(anglebreaks / 180 * pi))
+  sunangle <- sunangle / 180 * pi
+
+  lim.x <- if(sinsun < 0) 1 else nrow(heightmap)
+  lim.y <- if(cossun < 0) 1 else ncol(heightmap)
+
+  coords.x <- rep(seq_len(nrow(heightmap)), ncol(heightmap))
+  coords.y <- rep(seq_len(ncol(heightmap)), each=nrow(heightmap))
+
+  # For each coordinate, deterimine maximum distance towards light source before
+  # we fall off grid
+
+  dists <- as.integer(
+    pmin((lim.x - coords.x) / sinsun, (lim.y - coords.y) / cossun, maxsearch)
+  )
+  max.dist <- max(dists)
+
+  # We need to compute the coordinates along the path to light source.  To do
+  # this generate one vector that will contain concatenated the paths for each
+  # of the original points.
+
+  sin.dists <- seq_len(max.dist) * sinsun
+  cos.dists <- seq_len(max.dist) * cossun
+
+  # coords.id tracks the origin coordinate of each element in our light path
+
+  coords.id <- rep(seq_along(coords.x), dists)
+  coords.offset <- sequence(dists)
+  coords.x.id <- coords.x[coords.id]
+  coords.y.id <- coords.y[coords.id]
+
+  coords.l.x <- coords.x.id + sin.dists[coords.offset]
+  coords.l.y <- coords.y.id + cos.dists[coords.offset]
+
+  # compute angular height at each of the light coords
+
+  heights.ang <- (
+    ff_bilin(heightmap, coords.l.x, coords.l.y) -
+    heightmap[cbind(coords.x.id, coords.y.id)]
+  ) / coords.offset
+
+  # compute max ang for each coord id, and then determine how many of the
+  # anglebreaks are above that.
+
+  ang.max <- tapply(heights.ang, coords.id, max)
+  matrix(
+    1 - (findInterval(ang.max, anglebreaks) / length(anglebreaks)),
+    dim(heightmap)
+  )
+}
+#' Bilinear Interpolation
+#'
+#' A vectorized version of Wolf Vollprecht's faster_bilinear (see
+#' R/slow-shade.R).
+
+ff_bilin <- function(Z, x, y) {
+  i <- as.integer(x)
+  j <- as.integer(y)
+  XT <- x - i
+  YT <- y - j
+
+  # Expand matrix to handle OOB cases; algorithm as written uses zero
+  # (implicitly), and so do we, but might be better to just use last row and
+  # column values instead.  We have not tested whether the additional allocation
+  # for the matrix is slower than trying to explicitly compute each of the
+  # special cases.
+
+  Z <- rbind(cbind(Z, 0), 0)
+
+  # Create symbols for re-used vectors
+
+  XT_1 <- 1 - XT
+  YT_1 <- 1 - YT
+  i1 <- i + 1L
+  j1 <- j + 1L
+
+  # compute interpolation in a vectorized manner
+
+  (YT_1) * (XT_1) * Z[cbind(i,  j)] +
+  (YT_1) * (XT)   * Z[cbind(i1, j)] +
+  (YT)   * (XT_1) * Z[cbind(i,  j1)] +
+  (YT)   * (XT)   * Z[cbind(i1,  j1)]
+}
---
+++ R/slow-shade.R
@@ -0,0 +1,80 @@
+# A simple bilinear interpolation
+#
+# Copyright: Wolf Vollprecht
+# License: GPL-3
+
+faster_bilinear <- function (Z, x0, y0){
+  i = floor(x0)
+  j = floor(y0)
+  XT = (x0 - i)
+  YT = (y0 - j)
+  result = (1 - YT) * (1 - XT) * Z[i, j]
+  nx = nrow(Z)
+  ny = ncol(Z)
+  if(i + 1 <= nx){
+    result = result + (1-YT) * XT * Z[i + 1, j]
+  }
+  if(j + 1 <= ny){
+    result = result + YT * (1-XT) * Z[i, j + 1]
+  }
+  if(i + 1 <= nx && j + 1 <= ny){
+    result = result + YT * XT * Z[i + 1, j + 1]
+  }
+  result
+}
+#' Original Pure Base R Version of rayshader
+#'
+#' This is based on the original Tyler Morgan Wall [blog
+#' post](http://www.tylermw.com/throwing-shade/), with some clean-up to
+#' functionalize, and with Wolf Vollprecht's bilinear interpolation
+#' [adaptation](https://nextjournal.com/wolfv/how-fast-is-r-with-fastr-pythran)
+#' function.
+#'
+#' inheritParams rayshader::ray_shade
+#' @value numeric matrix of shadow intensities between with values in
+#'   &91;0,1&93;.
+
+ray_shade1 <- function(
+  heightmap, anglebreaks=seq(40, 50, 1), sunangle=315, maxsearch=100
+) {
+  anglebreaks <- sort(tan(anglebreaks / 180 * pi))
+  sunangle <- sunangle / 180 * pi
+  maxdistance <- min(
+    floor(sqrt(ncol(heightmap)^2 + nrow(heightmap)^2)), maxsearch
+  )
+  cossun <- cos(sunangle)
+  sinsun <- sin(sunangle)
+  heightmapshadow = matrix(1, ncol = ncol(heightmap), nrow = nrow(heightmap))
+
+  for (i in 1:nrow(heightmap)) {
+    for (j in 1:ncol(heightmap)) {
+      vij = heightmap[i, j]
+      for (anglei in anglebreaks) {
+        for (k in 1:maxdistance) {
+
+          xcoord = (i + sinsun*k)
+          ycoord = (j + cossun*k)
+
+          if(xcoord > nrow(heightmap) ||
+             ycoord > ncol(heightmap) ||
+             xcoord < 0 || ycoord < 0) {
+            break
+          } else {
+            tanangheight = vij + anglei * k
+            if (all(c(heightmap[ceiling(xcoord), ceiling(ycoord)],
+                      heightmap[floor(xcoord), ceiling(ycoord)],
+                      heightmap[ceiling(xcoord), floor(ycoord)],
+                      heightmap[floor(xcoord), floor(ycoord)]) < tanangheight))
+              next
+            if (tanangheight < faster_bilinear(heightmap, xcoord, ycoord)) {
+              heightmapshadow[i, j] =  heightmapshadow[i, j] - 1 /
+                length(anglebreaks)
+              break
+            }
+          }
+        }
+      }
+    }
+  }
+  heightmapshadow
+}
